a615b592072fd87accf56781c04baa474ca96bee
banhbao0311 <nhan.nguyenthanh0311@gmail.com>
2023-10-26 12:19:38 +0700

    responsive accessbility

--- src/components/Accessibility/Accessibility.js
+++ src/components/Accessibility/Accessibility.js
@@ -3,16 +3,17 @@ import image from "./image.webp";
 import imgae2 from "./image2.webp";
 import image3 from "./image3.webp";
 import image4 from "./image4.webp";
+import OIP from "./OIP.jpg";
 const Container = styled.div `
   & .container {
-    margin: auto;
-    width: 1000px;
+    margin: 0 auto;
+    max-width: 1000px;
   }
 `;
 const Box1 = styled.div `
-  & .container {
-    /* margin: auto; */
-    width: 900px;
+  & .container {    
+    margin: 0 auto;
+    max-width: 900px;
   }
 
   & .h1-box1 {
@@ -35,10 +36,18 @@ const Box1 = styled.div `
 `;
 
 const Box2 = styled.div `
-  & .grid-container {
+    margin: 0 auto;
+    max-width: 1000px;
+  & .grid-container {    
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     grid-gap: 4em;
+  
+    @media  (max-width: 600px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+  
+  
   }
 
   & .h1-box2 {
@@ -59,6 +68,9 @@ const Box2 = styled.div `
     cursor: pointer;
     padding: 10px;
   }
+  & img{
+
+  }
 `;
 
 const Box3 = styled.div `
@@ -67,8 +79,22 @@ const Box3 = styled.div `
     grid-template-columns: repeat(2, 1fr);
     grid-gap: 10em;
     /* background-color: rgb(247,247,247); */
-    width: 1000px;
+    max-width: 1000px;
     margin: auto;
+
+    @media  (max-width: 600px) {
+    grid-template-columns: repeat(1, 1fr);
+    grid-gap: 0;
+
+  & .gird-item1 {
+    order: 2; /* Đảo ngược thứ tự của mục 1 */
+  }
+  & .gird-item2 {
+    order: 1; /* Đảo ngược thứ tự của mục 1 */
+  }
+  }
+
+
   }
 
   & .div1-box3 {
@@ -101,12 +127,22 @@ const Box4 = styled.div `
     height: 80vh;
     text-align: center;
     color: #fff;
+
+    @media  (max-width: 600px) {
+    padding-top: 30%;
+      
   }
+  }
+  & img{
+    max-width: 100%;
+  }
+
+
 `;
 
 const Box5 = styled.div `
   & .container-box5 {
-    width: 1200px;
+    max-width: 1200px;
     margin: auto;
     /* background-color: black;
     color: white; */
@@ -115,6 +151,12 @@ const Box5 = styled.div `
     display: grid;
     grid-template-columns: repeat(3,1fr);
     grid-gap: 2em;
+
+
+    @media  (max-width: 600px) {
+    grid-template-columns: repeat(1, 1fr);
+      
+  }
   }
   & .h1-box5{
     font-size: 2em;
@@ -128,7 +170,7 @@ const Box5 = styled.div `
 
   & .title-box5{
     font-size: 3em;
-    width: 1200px;
+    max-width: 1200px;
     padding-top: 1em;
     padding-bottom: 1em;
   }
@@ -138,81 +180,75 @@ const Box5 = styled.div `
     font-weight: bolder;
     text-align: left;
     width: 50%;
+    margin-top: 3%;
+    margin-bottom: 3%;
+    @media  (max-width: 600px) {
+    width: 100%;
+    
   }
+  }
+
+
 
 `;
 
 function Accessibility() {
     return (
         <Container>
-            <div className="container">
-                <Box1>
-                    <div className="container">
-                        <h1 className="h1-box1">Accessibility at Airbnb</h1>
-                        <p className="p1-box1">
-                            Here’s how we’re making it easier to travel with us.
+
+            <Box1>
+                <div className="container">
+                    <h1 className="h1-box1">Accessibility at Airbnb</h1>
+                    <p className="p1-box1">
+                        Here’s how we’re making it easier to travel with us.
+                    </p>
+                </div>
+            </Box1>
+            <Box2>
+                <div className="grid-container">
+                    <div className="div1-box2">
+                        <h1 className="h1-box2">The Adapted Category</h1>
+                        <p className="p1-box2">
+                            Discover unique homes with verified accessibility features  including step-free paths into the home, bedroom and bathroom.
+                                                                                                                                                                                                                                                             Each home in this category receives a detailed 3D scan to confirm its accessibility features and display key details like  doorway widths.
                         </p>
+                        <button className="button-box2">Start Exploring</button>
                     </div>
-                </Box1>
-                <Box2>
-                    <div className="grid-container">
-                        <div className="div1-box2">
-                            <h1 className="h1-box2">The Adapted Category</h1>
-                            <p className="p1-box2">
-                                Discover unique homes with verified accessibility features  including step-free paths into the home, bedroom and bathroom.
-                                                                                                                                 Each home in this category receives a detailed 3D scan to confirm its accessibility features and display key details like  doorway widths.
-                            </p>
-                            <button className="button-box2">Start Exploring</button>
-                        </div>
-                        <div className="div1-box2">
-                            <img src={image}/>
-                        </div>
+                    <div className="div1-box2">
+                        <img src={image}/>
                     </div>
-                </Box2>
-            </div>
+                </div>
+            </Box2>
+
             <Box3>
                 <div className="background-color"
                     style={
                         {backgroundColor: "rgb(247,247,247)"}
                 }>
                     <div className="grid-container">
-                        <div className="gird-column">
+                        <div className="gird-item1">
                             <img src={imgae2}/>
                         </div>
-                        <div className="gird-column2">
+                        <div className="gird-item2">
                             <h1 className="h1-box3">Enhanced search filters</h1>
                             <p className="p1-box3">
                                 We’ve simplified our accessibility filters to provide an even
-                                                                                                                                                                                                                                                                                better search experience.
+                                                                                                                                                                                                                                                                                                                                                                                                                better search experience.
                             </p>
                         </div>
                     </div>
                 </div>
-            </Box3>
+            </Box3>            
             <Box2>
-                <div style={
-                    {
-                        margin: "auto",
-                        width: "1000px"
-                    }
-                }>
-                    <div className="grid-container">
-                        <div style={
-                            {
-                                display: "flex",
-                                justifyContent: "center",
-                                flexDirection: "column"
-                            }
-                        }>
-                            <h1 className="h1-box2">The Adapted Category</h1>
-                            <p className="p1-box2">
-                                Discover unique homes with verified accessibility features                      doorway widths.
-                            </p>
-                            <button className="button-box2">Start Exploring</button>
-                        </div>
-                        <div>
-                            <img src={image3}/>
-                        </div>
+                <div className="grid-container">
+                    <div className="div1-box2">
+                        <h1 className="h1-box2">Accessibility review</h1>
+                        <p className="p1-box2">
+                        We review every accessibility feature submitted by stays Hosts for accuracy.  </p>
+                        <button className="button-box2">Start Exploring</button>
+                    </div>
+                    <div className="div1-box2">
+                        <img src={image3}/>
                     </div>
                 </div>
             </Box2>
@@ -222,14 +258,14 @@ function Accessibility() {
                         {backgroundColor: "rgb(247,247,247)"}
                 }>
                     <div className="grid-container">
-                        <div className="gird-column">
+                        <div className="gird-item1">
                             <img src={imgae2}/>
                         </div>
-                        <div className="gird-column2">
-                            <h1 className="h1-box3">Enhanced search filters</h1>
+                        <div className="gird-item2">
+                            <h1 className="h1-box3">1:1 messaging with Hosts</h1>
                             <p className="p1-box3">
-                                We’ve simplified our accessibility filters to provide an even
-                                                                                                                                                                                                                                                                                better search experience.
+                            Chat directly with Hosts for more information about the accessibility features of their stay or Experience.
+                                                                                                                                                                                                                                                                                                                                                                                                                better search experience.
                             </p>
                         </div>
                     </div>
@@ -255,8 +291,8 @@ function Accessibility() {
                                 <h1 className="h1-box5">Dedicated teams</h1>
                                 <p className="p1-box5">
                                     Airbnb has teams focused on building products that everyone can
-                                                                                                                                                                                                                                                                                                        use. These teams work with engineers, designers, and others
-                                                                                                                                                                                                                                                                                                        across the compazzzzz
+                                                                                                                                                                                                                                                                                                                                                                                                                                                        use. These teams work with engineers, designers, and others
+                                                                                                                                                                                                                                                                                                                                                                                                                                                        across the compazzzzz
                                 </p>
                             </div>
                             <div className="grid-item">
@@ -269,14 +305,14 @@ function Accessibility() {
                                 <h1 className="h1-box5">Dedicated teams</h1>
                                 <p className="p1-box5">
                                     Airbnb has teams focused on building products that everyone can
-                                                                                                                                                                                                                                                                                                        use. These teams work with engineers, designers, and others
-                                                                                                                                                                                                                                                                                                        across the compazzzzz
+                                                                                                                                                                                                                                                                                                                                                                                                                                                        use. These teams work with engineers, designers, and others
+                                                                                                                                                                                                                                                                                                                                                                                                                                                        across the compazzzzz
                                 </p>
                             </div>
                         </div>
 
                         <h1 className="h2-box5">Here are some of the organizations that we partner with</h1>
-
+                        <img src={OIP}/>
 
                     </div>
                 </div>
